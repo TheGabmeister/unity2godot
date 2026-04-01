312b58f303670433baea0b8f0cac12edcf5a4900
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-04-01 08:51:15 +0800

    Add simple hello world

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "ufbx.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "ufbx version: " << UFBX_VERSION << std::endl;
+    return 0;
+}
